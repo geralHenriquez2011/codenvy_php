e0e0899467e21df24d56bebbd5677aa0cdcbc52f
geralHenriquez2011 <geral.henriquez2011@gmail.com>
2013-08-27 10:46:47 -0400

    Update index.php

--- index.php
+++ index.php
@@ -1,8 +1,38 @@
 <html>
-  <head>
-     <title>Hello World</title>
-  </head>
-  <body>
-     <?php echo '<p>Hello World</p>'; ?>
-  </body>
+ <head>
+     <meta http-equiv="Content-Type"content="text/html; charset=UTF-8"/>
+     <title>scorm</title>     
+     
+     <script src="http://code.jquery.com/jquery-1.9.1.js"></script>
+     
+     
+   </head>
+   <body>
+     
+     
+     
+     <?php
+//incluye libreria zip
+require_once("pclzip.lib.php"); 
+
+//estandar de configuracion funciones y metodos
+include "config/config.php";      //***
+include "config/metodos.php";     //***
+include "config/function.php";    //***
+
+
+
+     ?>
+     <!-- The data encoding type, enctype, MUST be specified as below -->
+     <form id="form" enctype="multipart/form-data" method="POST">
+       
+       Cargar Archivo:  <input name="userfile" id="nombre_archivo" type="file" />
+      	
+       <input type="submit" value="Upload" />
+     </form>
+     
+     
+     
+   </body>
+
 </html>
